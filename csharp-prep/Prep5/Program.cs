f37b1f0eebe4e478a713902a189cb31ce75aa48b
Ivan Rosso <accurateivan@gmail.com>
2026-05-13 07:15:49 -0600

    A program that displays results based on input of personal user info.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
+    static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int GetUserNumber()
+    {
+        Console.Write("What is your favorite number?: ");
+        int fav_number = int.Parse(Console.ReadLine());
+        return fav_number;
+
+    }
+
+    static void GetUserBirthYear(out int birthYear)
+    {
+        Console.Write("Please enter your birth year: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number*number;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int square, int userBirthYear)
+    {
+        int age = 2026 - userBirthYear;
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+        Console.WriteLine($"{userName}, you will turn {age} this year.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string userName = GetUserName();
+        int userNumber = GetUserNumber();
+        int birthYear;
+        GetUserBirthYear(out birthYear);
+        int square = SquareNumber(userNumber);
+        DisplayResult(userName, square, birthYear);
     }
 }
