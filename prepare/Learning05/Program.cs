4af93528f5fa97247eed104e08f5877a7d3507e7
Ivan Rosso <accurateivan@gmail.com>
2026-06-18 20:03:24 -0600

    W09 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Build a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 4));
+        shapes.Add(new Rectangle("blue", 5, 3));
+        shapes.Add(new Circle("green", 7));
+
+        // Iterate and display each shape's color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()},  Area: {shape.GetArea()}");
+        }
     }
 }
