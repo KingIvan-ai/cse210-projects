dccd7c9f885e004da50f813c4b9a20e17a4b94e9
Ivan Rosso <accurateivan@gmail.com>
2026-07-13 21:06:29 -0600

    Add main program that creates a list of shapes and displays each one

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Triangle("Green", 3, 4, 3, 4, 5));
+
+        foreach (Shape s in shapes)
+        {
+            s.Display();
+            Console.WriteLine();
+        }
     }
 }
