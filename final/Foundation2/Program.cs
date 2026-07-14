1e7874654ffdea1d34dc8b3882b6f040af782052
Ivan Rosso <accurateivan@gmail.com>
2026-07-13 20:42:41 -0600

    Add main program that creates a bank account and tests deposits and withdrawals

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        BankAccount account = new BankAccount("Ivan", 1000.00);
+
+        account.DisplayInfo();
+        Console.WriteLine();
+
+        account.Deposit(500.00);
+        Console.WriteLine("Deposited $500.00");
+
+        account.Withdraw(200.00);
+        Console.WriteLine("Withdrew $200.00");
+
+        Console.WriteLine();
+        account.DisplayInfo();
     }
 }
